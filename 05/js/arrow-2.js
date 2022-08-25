67b0f8782ae4230b836d8388c3f1c93fa81d3828
윤진수 <yjs104710@gmail.com>
2022-08-26 04:22:06 +0900

    event-dom

---
+++ 05/js/arrow-2.js
@@ -0,0 +1,3 @@
+let hi = user => {document.write (user + "님, 안녕하세요?")};
+
+hi("진수");
--- 05/js/local.js
+++ 05/js/local.js
@@ -1,7 +1,7 @@
-var myVar = 100;
+var myVar = 100; //전역변수 선언
 test();
 document.write("myVar is " + myVar);
 
 function test() {
-    var myVar = 50;
+    var myVar = 50; //지역변수 선언
 }
