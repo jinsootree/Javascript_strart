a07ce649eeb93f1b695bb438c95e1f2a323713f8
윤진수 <yjs104710@gmail.com>
2022-08-31 23:21:17 +0900

    firstDay

---
+++ 06/js/dday-result.js
@@ -0,0 +1,10 @@
+var now = new Date(); //오늘 날짜 정보를 date 객체의 인스턴스 now 객체로 만듬
+var firstDay = new Date("2018-03-23") //첫째날의 날짜 정보를 firstDay 객체로 만듬
+
+var toNow = now.getTime( );
+var toFirst = firstDay.getTime( );
+var passedTime = toNow - toFirst;
+
+var passedTime = Math.round( passedTime/(1000 * 60 * 60 * 24)) //밀리초를 날짜수로 변환한 후 올림
+
+document.querySelector("#accent").innerText = passedTime + "일";
