16239f8a36e534669e0e7b118242c450ca18340c
윤진수 <yjs104710@gmail.com>
2022-09-07 19:21:25 +0900

    domCss

--- 08/js/domevent.js
+++ 08/js/domevent.js
@@ -1,13 +1,18 @@
     var pic = document.querySelector('#pic');
     pic.addEventListener("mouseover",changePic,false);
     pic.addEventListener("mouseout",originPic,false);
-    document.addEventListener('click',function() {
-      alert("hello")
+    /*document.addEventListener('click',function() {
+      alert("안녕하세요")
     });
+    */
+   document.addEventListener("click",hello,false);
     
 		function changePic() {			
 			pic.src = "images/boy.png";
     }
     function originPic() {
       pic.src = "images/girl.png";
+    }
+    function hello() {
+      alert("안녕하세요")
     }
