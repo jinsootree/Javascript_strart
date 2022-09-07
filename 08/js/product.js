a29c235a278528c639db78eca2b353b023e80a5b
윤진수 <yjs104710@gmail.com>
2022-09-08 06:48:31 +0900

    상세설명

--- 08/js/product.js
+++ 08/js/product.js
@@ -1,3 +1,4 @@
+		
 		var bigPic = document.querySelector("#cup");  
 		var smallPics = document.querySelectorAll(".small");  
 
@@ -7,3 +8,20 @@
 				bigPic.setAttribute("src", newPic);
 			});
 		}
+		var isOpen = false;
+		var view = document.querySelector("#view");
+		view.addEventListener("click", function(){
+			if(isOpen == false){
+				document.querySelector("#detail").style.display = "block";
+				view.innerHTML = "상세설명 닫기";
+				isOpen = true;
+			}
+			else {
+				document.querySelector("#detail").style.display = "none";
+				view.innerHTML = "상세설명 보기";
+				isOpen = false;
+
+			}
+		});
+
+		
