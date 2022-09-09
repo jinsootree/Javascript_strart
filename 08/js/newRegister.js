93df6be062d26fa442b5cb7d02bd861cf85cbc56
윤진수 <yjs104710@gmail.com>
2022-09-09 10:55:01 +0900

    change

--- 08/js/newRegister.js
+++ 08/js/newRegister.js
@@ -1,10 +1,10 @@
-function newRegister () {
-    var newP = document.createElement("p");
-    var userName = document.querySelector("#userName");
-    var newText = document.createTextNode(userName.vaule);
-    newP.appendChild(newText);
+function newRegister() {
+  var newP = document.createElement("p");
+  var userName = document.querySelector("#userName");
+  var newText = document.createTextNode(userName.value);
+  newP.appendChild(newText);
 
-    var nameList = document.querySelector("#nameList");
-    nameList.appendChild(newP);
-    userName.vaule="";
+  var nameList = document.querySelector("#nameList");
+  nameList.appendChild(newP);
+  userName.vaule = "";
 }
